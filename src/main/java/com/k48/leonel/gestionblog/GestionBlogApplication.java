85a59fe645ce26834a633d2b0383f00e0e3c949d
nono leonel <ncleonel3@gmail.com>
2026-09-21 21:40:42 +0100

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/k48/leonel/gestionblog/GestionBlogApplication.java
@@ -0,0 +1,12 @@
+package com.k48.leonel.gestionblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBlogApplication.class, args);
+    }
+}
