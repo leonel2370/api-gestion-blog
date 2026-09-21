8fa632f20d9bb47315a4537b64c633f8988cf9b3
nono leonel <ncleonel3@gmail.com>
2026-09-21 21:41:10 +0100

    test: add Spring context load test

---
+++ src/test/java/com/k48/leonel/gestionblog/GestionBlogApplicationTests.java
@@ -0,0 +1,15 @@
+package com.k48.leonel.gestionblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Vérifie que le contexte Spring démarre correctement (base H2 en mémoire).
+ */
+@SpringBootTest
+class GestionBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
